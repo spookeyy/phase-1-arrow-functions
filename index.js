f1d1d728be5aeecaf4880a85d3bd35abeb1977e4
spookeyy <pangasmeshack@gmail.com>
2024-03-23 19:45:11 +0300

    complete arrow functions lab

---
+++ index.js
@@ -0,0 +1,11 @@
+//Arrow function declaration
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// add(2,3); //=> 5
+
+const twoAdder = x => x + 2;
+// is the same as
+const twoAdder1 = (x) => x + 2;
+
+const divide = (a, b) => a / b;
+const square = x => x * x;
+const add = (a, b) => a + b;
